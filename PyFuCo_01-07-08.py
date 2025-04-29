9603586b7ec2cb6a3da6dcdc97307db68a5509bb
reisenhardt <ralph.eisenhardt@gmx.de>
2025-04-29 19:13:27 +0200

    Erster Upload

---
+++ PyFuCo_01-07-08.py
@@ -0,0 +1,7 @@
+has_high_income = True
+has_good_credit = True
+
+if has_high_income & has_good_credit:
+    print("Du Schlawiner, läuft bei Dir, oder?")
+else:
+    print(f"Musch halt mal was schaffa!")
