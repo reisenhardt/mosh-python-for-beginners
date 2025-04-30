d296f2afb71c18ada0f8df0e9ad8c20bd73f43c9
reisenhardt <ralph.eisenhardt@gmx.de>
2025-04-30 09:10:00 +0200

    Update 30.04.25

---
+++ PyFuCo_01-24-36.py
@@ -0,0 +1,12 @@
+number = 9
+i = 1
+guess_number = ""
+while i < 5:
+    guess_number = int(input("Guess a number: "))
+    i += 1
+    if guess_number == number:
+        print("Der Wahnsinn, Du hast es geschafft!")
+        break
+    print(f"Bitte noch einmal probieren! Du hast noch {5-i} Versuche!")
+
+print("Das war's. ")
